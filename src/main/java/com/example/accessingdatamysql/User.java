f3615f169f87d9c09c508546174b2cb06ead4622
Robert Spaleniak <robciopl25@gmail.com>
2023-03-31 19:48:15 +0200

    initial commit

---
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,72 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String surname;
+
+    private String password;
+
+    private String isLoggedIn;
+
+
+    public String getIsLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setIsLoggedIn(String isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
