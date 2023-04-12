10c60ed2438a16b7e2ac3edbc6b953a3908cb16e
Robert Spaleniak <robciopl25@gmail.com>
2023-04-12 16:57:13 +0200

    login endpoint fix

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,8 +2,10 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.accessingdatamysql"})
 public class AccessingDataMysqlApplication {
 
     public static void main(String[] args) {
